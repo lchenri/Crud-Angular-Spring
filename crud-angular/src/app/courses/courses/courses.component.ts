304b1971fc0b6ed289d52ace42abcf2965d2520d
lchenri <lucasbonicardoso@hotmail.com>
2023-07-27 20:55:13 -0300

    adicao de table com fake db

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-
+  //geralmente é feita a tipagem quando não iniciamos a variável!
+  courses: Course[] = [
+    {_id: "1", name: "Angular", category: "front-end"}
+  ];
+  displayedColumns = ['name', 'category'];
 }
